c4764e70a36fb48b36f3e3c8a6aeeb9a8de5020b
Juliabergstron <bergstronjuliacosta@gmail.com>
2025-08-26 21:46:04 -0300

    Add files via upload

---
+++ SwitchCaseExercicio3.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.switchcaseexercicio3;
+
+/**
+ *
+ * @author user
+ */
+import java.util.Scanner ;
+public class SwitchCaseExercicio3 {
+
+    public static void main(String[] args) {
+      try (Scanner scanner = new Scanner(System.in)) {
+       System.out.println("Digite um número de 1 a 7 e mostrarei o dia correspondente.");
+       int semanadia= scanner.nextInt();
+       
+       switch (semanadia) {
+          
+          case 1 -> System.out.println("Domingo");
+          case 2 -> System.out.println("Segunda");
+          case 3 -> System.out.println("Terça");
+          case 4 -> System.out.println("Quarta");
+          case 5 -> System.out.println("Quinta");
+          case 6 -> System.out.println("Sexta");
+          case 7 -> System.out.println("Sábado");
+          default -> System.out.println("Dia não compatível");
+         
+      }
+       
+       
+       
+      }         
+  }
+}
---
+++ SwitchCaseExercicios.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.switchcaseexercicios;
+
+/**
+ *
+ * @author user
+ */
+import java.util.Scanner ;
+public class SwitchCaseExercicios {
+
+    public static void main(String[] args) {
+      try (Scanner scanner = new Scanner(System.in)) {
+       System.out.println("Digite um número que seja 1, 2 ou 3 e mostrarei a fruta correspondente.");
+       int numerofruta = scanner.nextInt();
+       
+       switch (numerofruta) {
+          
+          case 1 -> System.out.println("A fruta correspondente é a maça");
+          case 2 -> System.out.println("A fruta correspondente é a banana");
+          case 3 -> System.out.println("A fruta correspondente é a laranja");
+          default -> System.out.println("Não há fruta correspondente.");
+         
+      }
+       
+       
+       
+      }         
+  }
+}
